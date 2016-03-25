c6d516d7ee3c263faf36c7df87eb3224f7ca7a5e
grayworm <fullyou-210@163.com>
2016-03-25 11:08:02 +0800

    init

---
+++ src/main/scala/Test.scala
@@ -0,0 +1,19 @@
+/**
+ * Created by liugang on 16-3-4.
+ */
+object Test {
+  def main(args: Array[String]) {
+    //val Array(test, tt) = args
+    var line = ""
+    val tt = "3"
+    if (tt == 1) {
+      line = "one"
+      print("hell world " + line)
+    }
+    if (tt == 3) {
+      line = "two"
+      print("hell world " + line)
+    }
+
+  }
+}
